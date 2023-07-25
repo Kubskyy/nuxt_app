d17b08d6ec921cb87663d42ca5c8aa465ba6bb07
Jakub Rutkowski <kubaa.rutkowski123@gmail.com>
2023-07-25 20:31:01 +0200

    add UserList component

---
+++ pages/about.vue
@@ -0,0 +1,19 @@
+<!--
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+-->
+<template>
+<h1>test</h1>
+</template>
+<script setup></script>
+  
