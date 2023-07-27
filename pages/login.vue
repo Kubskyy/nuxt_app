3a0fa67eafbf6713b95d7d4be110325ec2f0a4f9
Jakub Rutkowski <kubaa.rutkowski123@gmail.com>
2023-07-27 12:48:36 +0200

    fix tsconfig.json

--- pages/login.vue
+++ pages/login.vue
@@ -1,29 +1,6 @@
 <template>
-    <!--
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
--->
-<!--
-  This example requires updating your template:
-
-  ```
-  <html class="h-full bg-white">
-  <body class="h-full">
-  ```
--->
 <div class="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
   <div class="sm:mx-auto sm:w-full sm:max-w-sm">
-    <GithubIcon />
     <h2 class="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
   </div>
 
