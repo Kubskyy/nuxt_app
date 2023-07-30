ef0fe4c8cd3c7d5350fe62f6d804c75f1e60f1ab
Jakub Rutkowski <kubaa.rutkowski123@gmail.com>
2023-07-30 16:58:28 +0200

    add component for edit view, edit user save format

--- components/User/UserListItem.vue
+++ components/User/UserListItem.vue
@@ -1,6 +1,6 @@
 <template>
     <tr>
-        <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">{{user.name}}</td>
+        <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-0">{{user.name}} {{user.surname}}</td>
         <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{user.title}}</td>
         <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{user.email}}</td>
         <td class="whitespace-nowrap px-3 py-4 text-sm text-gray-500">{{user.role}}</td>
--- server/api/users.js
+++ server/api/users.js
@@ -1,8 +1,8 @@
 export default defineEventHandler((event) => {
     return {
       users: [
-        {id:1, name:"Lindsay Matt", title:"Front-end Dev", email:"email@email.com", role:"member"},
-        {id:2, name:"Matt Kidney", title:"Back-end Dev", email:"email@email.com", role:"member"},
-        {id:3, name:"Adam Sandler", title:"Front-end Dev", email:"email@email.com", role:"member"}]
+        {id:1, name:"Lindsay", surname:"Matt",  title:"Front-end Dev", email:"email@email.com", role:"member"},
+        {id:2, name:"Matt", surname:"Kidney", title:"Back-end Dev", email:"email@email.com", role:"member"},
+        {id:3, name:"Adam", surname:"Sandler",  title:"Front-end Dev", email:"email@email.com", role:"member"}]
     }
   })
