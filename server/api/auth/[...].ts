7cc3b13b05d362ab1e2ebd4dcd2d398ce30205ae
Jakub Rutkowski <kubaa.rutkowski123@gmail.com>
2023-07-31 10:31:35 +0200

    add environment variable

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -8,8 +8,8 @@ export default NuxtAuthHandler({
     providers: [
         // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
         GithubProvider.default({
-           clientId: '73b6773f4e4a5e8040c2',
-           clientSecret: '66418bf61c61b232339601873cdd786d3bfdab2f'
+           clientId: process.env.GITHUB_CLIENT_ID,
+           clientSecret: process.env.GITHUB_CLIENT_SECRET
         }),
         CredentialsProvider.default({
             name: 'Credentials',
